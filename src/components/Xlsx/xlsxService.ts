3f9b93df506eb85a3eca563f556a8dac85ac5cf9
logicsculptor0117 <logicsculptor0117@gmail.com>
2024-06-08 03:39:37 +0900

    ADD: publish excel

---
+++ src/components/Xlsx/xlsxService.ts
@@ -0,0 +1,16 @@
+import * as Xlsx from "xlsx";
+import fs from "fs";
+import { getAllAddress } from "../Tyrh/tyrhModel";
+import { TyrhInterface } from "../Tyrh/tyrhInterface";
+
+const writeXlsx = async () => {
+  const results: TyrhInterface[] = await getAllAddress();
+
+  const workSheet = Xlsx.utils.json_to_sheet(results);
+  const workbook = Xlsx.utils.book_new();
+  Xlsx.utils.book_append_sheet(workbook, workSheet, "Sheet1");
+  Xlsx.writeFile(workbook, "output.xlsx");
+  console.log(`Excel file written successfully`);
+};
+
+export { writeXlsx };
--- src/startup/events.ts
+++ src/startup/events.ts
@@ -2,12 +2,14 @@ import tyrhEventStart from "../components/Tyrh/tyrhEvent";
 import stakingEventStart from "../components/Staking/stakingEvent";
 import plantationEventStart from "../components/Plantation/plantationEvent";
 import nftEventStart from "../components/Nft/nftEvent";
+import {writeXlsx} from "../components/Xlsx/xlsxService";
 
 const eventMonitorStart = async () => {
-  await tyrhEventStart();
+  // await tyrhEventStart();
   // await stakingEventStart();
   // await plantationEventStart();
   // await nftEventStart();
+  await writeXlsx();
 
   console.log(`Event end`);
 };
