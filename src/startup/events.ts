191f995cb486e6e8c9bf3d32606db4426f57435d
logicsculptor0117 <logicsculptor0117@gmail.com>
2024-06-07 17:47:36 +0900

    FIX: a bit

--- src/startup/events.ts
+++ src/startup/events.ts
@@ -2,10 +2,10 @@ import tyrhEventStart from "../components/Tyrh/tyrhEvent";
 import stakingEventStart from "../components/Staking/stakingEvent";
 import plantationEventStart from "../components/Plantation/plantationEvent";
 
-const eventMonitorStart = () => {
-  tyrhEventStart();
-  // stakingEventStart();
-  // plantationEventStart();
+const eventMonitorStart = async () => {
+  await tyrhEventStart();
+  await stakingEventStart();
+  await plantationEventStart();
 };
 
 export default eventMonitorStart;
